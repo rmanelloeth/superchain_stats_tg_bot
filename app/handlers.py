57e8cda29ad8e121048cf849cc9a47d2838fe76d
rmanello <topartist2021@gmail.com>
2025-06-03 16:50:26 +0300

    Рабочий код для base

--- app/handlers.py
+++ app/handlers.py
@@ -1,10 +1,12 @@
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
 from aiogram import Router, F
+from aiogram.fsm.context import FSMContext
 
 import app.keyboards as kb
 
 from app.utils import get_base_tx_count
+from app.states import WalletState
 
 router = Router()
 
@@ -14,6 +16,12 @@ async def cmd_start(message: Message):
                          "пришли номер кошелька", reply_markup=kb.main)
 
 @router.message(F.text == 'wallet')
-async def cmd_wallet(message: Message):
-    txs, wallet = await get_base_tx_count('0xA795ecc5bF12A45C7F99ac16870d941D2c9Cd6dA')
-    await message.reply(text=f'Количество транзакций в сети Base: {txs} на кошельке {wallet}  ')
+async def cmd_wallet(message: Message, state: FSMContext):
+    await state.set_state(WalletState.address)
+    await message.answer('Input wallet')
+
+@router.message(WalletState.address)
+async def get_wallet_info(message: Message, state: FSMContext):
+    await state.update_data(address = message.text)
+    tx_count = await get_base_tx_count(message.text)
+    await message.answer(text=f'Количество транзакций в сети Base: {tx_count} на кошельке {message.text}')
