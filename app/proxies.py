8b1ec1f667dc98ddfc10185657108c3682a5ebc9
Oleg <120922304+rmanelloeth@users.noreply.github.com>
2025-09-10 18:39:34 +0300

    Update proxies.py

--- app/proxies.py
+++ app/proxies.py
@@ -1,8 +1,1 @@
-proxies = ['45.159.54.189:7061:fybzfybe:5bmb1fw89uak',
-            '92.113.115.3:5950:fybzfybe:5bmb1fw89uak',
-            '45.150.176.60:5933:fybzfybe:5bmb1fw89uak',
-            '168.199.244.234:6766:fybzfybe:5bmb1fw89uak',
-            '204.217.161.41:6613:fybzfybe:5bmb1fw89uak',
-            '45.146.31.156:5743:fybzfybe:5bmb1fw89uak',
-            '45.12.179.208:6739:fybzfybe:5bmb1fw89uak']
 
